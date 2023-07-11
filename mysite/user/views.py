04abd1ced72c11408e1422be2d2415a03669fccd
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2023-07-11 09:51:15 -0400

    Hola Simon

---
+++ mysite/user/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+def user(request):
+    return render(request, 'login.html', {})
