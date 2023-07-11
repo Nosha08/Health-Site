2e96d1ae9cf842395d132724c51eae9739a6debe
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-07-11 11:54:01 -0400

    dfgdss
    sfsdsfsf

--- mysite/calend/models.py
+++ mysite/calend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Appointment(models.Model):
+    time = models.IntegerField()
+    date = models.DateField()
+    def __str__(self):
+        return "Date: " + self.date + " Time: " + self.time + ":00"
--- mysite/calend/views.py
+++ mysite/calend/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-
+from .forms import AppointmentForm
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the calendar index!")
+
+def create(request):
+    render(request, 'templates/make_appointment.html',{})
