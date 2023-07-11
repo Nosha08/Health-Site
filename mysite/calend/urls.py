f905dda8197b0e05aaacca9f5fd713f892f3ab78
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-07-11 12:55:16 -0400

    dfsbfdgntrytryytr
    jhgtfffgdndhyrte

--- mysite/calend/urls.py
+++ mysite/calend/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("make_appointment/", views.create, name = "create")
+    path("make_appointment/", views.create, name = "create"),
 ]
