91240ecf46ded7d978e53400b9138cfa4fdaf935
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2023-07-13 10:32:17 -0400

    Progress

--- mysite/medical/views.py
+++ mysite/medical/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-import requests
-from requests.structures import CaseInsensitiveDict
 
 def index(request):
     return render(request, 'index.html', {})
@@ -8,12 +6,12 @@ def index(request):
 
 def form(request):
     if request.method == 'POST':
-        text = request.POST.get('location')
-        api_key = '2f66e0d2e25a40ba8a098c2c3a03d727'
-        url = f'https://api.geoapify.com/v1/geocode/autocomplete?text={text}&type=postcode&apiKey={api_key}'
+        name = request.POST.get('name')
+        open = request.POST.get('open')
+        close = request.POST.get('close')
+        location = request.POST.get('location')
+        print(name)
 
-        response = requests.get(url)
-        data = response.json()
-        # Process the response data as needed
-
-        return render(request, 'form.html', {'data': data})
+        return render(request, 'form.html', {'name': name, 'open': open, 'close': close, 'location': location})
+    
+    return render(request, 'form.html', {})
