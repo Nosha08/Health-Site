7ebe595deca290662e84bc733bf5bc8d3ff9663a
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-07-18 13:00:20 -0400

    sdf
    sdfsdf

--- mysite/calend/views.py
+++ mysite/calend/views.py
@@ -78,22 +78,21 @@ def create(request, year=2023, month="January", day = 1):
         if "submitted" in request.GET:
             submitted = True
     appointments_all = Appointment.objects.all().filter(date = send_date)
+    appointments_month = Appointment.objects.all().filter(month = month_number, year = year)
+
     times = []
     times_len = len(TIME_LIST)
     for objects in appointments_all:
         times.append(objects.time)
 
+    filled = []
+    for i in range(32):
+        filled.append(0)
 
-
-    for i in appointment_list:
-        Sdate = str(i.date)
-        print(Sdate)
-        separate = Sdate.split("-")
-        print(separate)
-        Smonth = separate[1]
-        Syear = separate[0]
-        print(Smonth)
-        print(Syear)
+    for j in appointments_month:
+        for i in range(32):
+            if j.day == i:
+                filled[i] += 1
     return render(request, 'calender.html',{
         "year":year,
         "month":month,
@@ -108,6 +107,7 @@ def create(request, year=2023, month="January", day = 1):
         "time_choices": TIME_CHOICES,
         "time_list": TIME_LIST,
         "appointments_all": appointments_all,
-        "times":times,
+        "times": times,
         "times_len": times_len,
+        "filled": filled,
         })
