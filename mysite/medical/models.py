782bfd3ce0cdb7f3721af8501dce70af9dc9f56e
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-07-31 11:51:39 -0400

    Update models.py

--- mysite/medical/models.py
+++ mysite/medical/models.py
@@ -15,8 +15,6 @@ class Office(models.Model):
     close = models.TimeField(auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    #reviews = models.PositiveIntegerField(default=0, null=True)
-
     def get_absolute_url(self):
         return reverse('results', args=[str(self.id)])
     
