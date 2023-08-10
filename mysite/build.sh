8fbcce3165d322daf8258dafc131296a45300bd3
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-08-10 08:10:35 -0400

    test5

---
+++ mysite/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
--- mysite/medical/tests.py
+++ mysite/medical/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
-
+import secrets
+print(secrets.token_hex(24))
 # Create your tests here.
