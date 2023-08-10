5b2b0cf96f0ca87724147faf07eb3924fb5ac878
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-08-10 08:27:33 -0400

    sdf

--- mysite/build.sh
+++ mysite/build.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-#pip install -r requirements.txt
+# Change the working directory to the root of your Django project
+cd C:/Users/simon.saffayeh/Documents/GitHub/Health-Site/mysite
+
+# Install dependencies
+poetry install
+
+# Run Django management commands
 python manage.py collectstatic --no-input
+python manage.py migrate
+
+# You can include other commands here if needed
